1fa8a78b5fb3e536cce7eb7183bacda8f122d2e6
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-06 15:05:07 +0530

    added operators, but skipped cause it's easy af

---
+++ cpp-things/02-operators-inputs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// subconsciously went in JS mode 😂
+//  const num1 = 12;
+//  const num2 = 2;
+
+// int num1 = 12;
+// int num2 = 2;
+int num1;
+int num2;
+
+int main() {
+    cout << "Gimme first number: ";
+    cin >> num1;
+    cout << "Gimme second number: ";
+    cin >> num2;
+    cout << "Addition of numbers is: " << num1 + num2 << endl;
+    cout << "Subtraction of numbers is: " << num1 - num2 << endl;
+    cout << "Multiplication of number is: " << num1 * num2 << endl;
+    cout << "Division of numbers is: " << num1 / num2 << endl;
+    cout << "remider after division is: " << num1 % num2 << endl;
+    return 0;
+}
+
+// tbh every operator is same as JS,
+// not going to spend time here as it's just for me
+
+// Maybe future me will add some imp things here
