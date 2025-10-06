343db7eaa48f2afd99dd83737d7163e362958a6e
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-06 13:50:01 +0530

    added data type file, on the basics rn

---
+++ cpp-things/01-data-types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+// 2 or 4 bytes
+int num = 7;
+
+// double is more precise than float
+// float 4 bytes, double 8 bytes
+float num2 = 4.8;
+double num3 = 4.9;
+
+// we can also use ASCII number if no quotes
+char letter = 'S';
+// char letter2 = "S"; wtf, it's just doesn't work ("")
+
+// not built in though
+string name = "Epicurious";
+
+// 1 byte like char
+bool isStudent = true;
+
+int main() {
+    // boolean reurns 0/1
+    std::cout << isStudent;
+}
+
+// There's a dude named "auto" which auto assigns data type.
--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "I don't know what I am doing, tbh." << endl;
-    return 0;
-}
