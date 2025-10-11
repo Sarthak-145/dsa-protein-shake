1f6c6cf30e75fc15c7b989b8bf28e1ad8f83c44c
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-11 23:00:06 +0530

    string things, nothing special yet

--- cpp-things/05-strings.cpp
+++ cpp-things/05-strings.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() { cout << "I'll say coming soon, even though I don't want to 🫥"; }
+// Strings are actually objects they say, I am reading doc so..
+// Like JS, but JS is more objetified 😂
+
+int main() {
+    string firstname = "Tyler ";
+    string lastname = "Durden";
+    string fullname = firstname + lastname;
+    // concatenation and append
+    cout << fullname << endl;
+    cout << firstname.append(lastname) << endl;
+
+    // for length
+    cout << lastname.size() << endl;
+    cout << lastname.length() << endl;
+
+    // both are somehow same, don't know the fukin point yet
+    cout << lastname[1] << endl;
+    cout << lastname.at(1) << endl;
+
+    string detail;
+    cout << "Tell something weird about you: \n";
+    getline(cin, detail);
+    cout << "LOL: " << detail;
+}
