fe697b47f30b8991c7af1bbe508748f899ad0c08
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-18 18:55:08 +0530

    kadane dude's algo is written and understood

--- arrays/05_kadanes_algorithm.cpp
+++ arrays/05_kadanes_algorithm.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // O(n^3) => boomer thing 😂
 int maxSubarray(int array[], int size) {
-    int maxsum = -1e6;
+    int maxsum = array[0];
 
     for (int i = 0; i < size; i++) {
         for (int j = i; j < size; j++) {
@@ -19,9 +19,54 @@ int maxSubarray(int array[], int size) {
     return maxsum;
 }
 
+int sameButBetter(int array[], int size) {
+    int maxsum = array[0];
+
+    for (int i = 0; i < size; i++) {
+        int sum = 0;
+        for (int j = i; j < size; j++) {
+            sum += array[j];
+            maxsum = max(sum, maxsum);
+        }
+    }
+    return maxsum;
+}
+
+// kadane dude's algo
+int sameAndBest(int array[], int size) {
+    int maxsum = array[0];
+    int currentsum = array[0];
+
+    for (int i = 0; i < size; i++) {
+        currentsum += array[i];
+
+        maxsum = max(maxsum, currentsum);
+
+        if (currentsum < 0) {
+            currentsum = 0;
+        }
+    }
+    return maxsum;
+}
+
 int main() {
     int numbers[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int size = sizeof(numbers) / sizeof(numbers[0]);
     cout << "Max sum of adjecent array elements is:\n ";
-    cout << maxSubarray(numbers, size);
+    cout << maxSubarray(numbers, size) << "\n\n";
+
+    // same but better function
+    cout << "Max sum of adjacent array elements is:\n";
+    cout << sameButBetter(numbers, size) << "\n\n";
+
+    // testing dude's algo 😎
+    cout << "Max sum of adjacent array elements is:\n";
+    cout << sameAndBest(numbers, size) << "\n\n";
+
+    // test with another me-made array, cause I've a doubt
+    int test[] = {-2, 7, -4, 5, 2, 0, -2, -4};
+    int sizetest = sizeof(test) / sizeof(test[0]);
+    cout << "Max sum of adjacent array elements is: \n";
+    cout << sameAndBest(test, sizetest) << "\n\n";
+    // Oh okay, doubt was dumb.
 }
