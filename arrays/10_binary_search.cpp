af7b45feb7659c3f1dadbf0a333806de6f371165
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-23 11:33:50 +0530

    study binary search with recursion, dr strange's mirrors

--- arrays/10_binary_search.cpp
+++ arrays/10_binary_search.cpp
@@ -22,6 +22,23 @@ int binarySearch(int array[], int size, int target) {
     return -1;
 }
 
+int recursive(int array[], int left, int right, int target) {
+    // edge case
+    if (left > right) {
+        return -1;
+    }
+    int mid = left + (right - left) / 2;
+
+    if (array[mid] == target) {
+        return mid;
+    } else if (array[mid] < target) {
+        return recursive(array, mid + 1, right, target);
+    } else {
+        return recursive(array, left, mid - 1, target);
+    }
+    return -1;
+}
+
 void printArray(int array[], int size) {
     for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
@@ -30,12 +47,16 @@ void printArray(int array[], int size) {
 }
 
 int main() {
-    // Array is pasted again from rotation file 😁
     int array[] = {12, 15, 26, 28, 31, 34, 42, 49, 57, 58, 80};
     int size = sizeof(array) / sizeof(array[0]);
     cout << "We're searching from the array: ";
     printArray(array, size);
     cout << "Number to be found is at the index: ";
     cout << binarySearch(array, size, 34);
+    cout << "\n\n";
+
+    // testing recursive
+    cout << "Searched by recursive binary search: ";
+    cout << recursive(array, 0, size - 1, 34);
     cout << "\n\n";
 }
