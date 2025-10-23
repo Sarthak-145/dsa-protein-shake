52101a2b5af37b00e3eb8334faf8ac286853af94
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-23 14:57:03 +0530

    study first and last occurence with binary search

--- arrays/10_binary_search.cpp
+++ arrays/10_binary_search.cpp
@@ -39,6 +39,53 @@ int recursive(int array[], int left, int right, int target) {
     return -1;
 }
 
+// more complexity
+//  int findFirstOcc(int array[], int size, int target) {
+//      for (int i = 0; i < size; i++) {
+//          if (array[i] == target) {
+//              return i;
+//          }
+//      }
+//      return -1;
+//  }
+
+int findFirstOcc(int array[], int size, int target) {
+    int start = 0;
+    int end = size - 1;
+    int result = -1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (array[mid] == target) {
+            result = mid;
+            end = mid - 1;
+        } else if (array[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return result;
+}
+
+int findLastOcc(int array[], int size, int target) {
+    int start = 0;
+    int end = size - 1;
+    int result = -1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (array[mid] == target) {
+            result = mid;
+            start = mid + 1;
+        } else if (array[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return result;
+}
+
 void printArray(int array[], int size) {
     for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
@@ -47,7 +94,7 @@ void printArray(int array[], int size) {
 }
 
 int main() {
-    int array[] = {12, 15, 26, 28, 31, 34, 42, 49, 57, 58, 80};
+    int array[] = {12, 15, 26, 28, 31, 34, 34, 34, 42, 49, 57, 58, 80};
     int size = sizeof(array) / sizeof(array[0]);
     cout << "We're searching from the array: ";
     printArray(array, size);
@@ -58,5 +105,16 @@ int main() {
     // testing recursive
     cout << "Searched by recursive binary search: ";
     cout << recursive(array, 0, size - 1, 34);
+    cout << "\n\n";
+
+    // test first occurence function
+    // If there are duplicates then this is useful, else -> this is shit.
+    cout << "index of the first occ of ele is: ";
+    cout << findFirstOcc(array, size, 34);
+    cout << "\n\n";
+
+    // test last occ
+    cout << "Index of the last occ of ele is: ";
+    cout << findLastOcc(array, size, 34);
     cout << "\n\n";
 }
