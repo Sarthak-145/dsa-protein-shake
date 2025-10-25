94fc853d12744fe7ad4f67d6adbd380078356896
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-25 19:39:07 +0530

    merging two sorted arrays into one result array

---
+++ arrays/11_array_manipulation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void mergeSortedArray(int array1[], int size1, int array2[], int size2,
+                      int result[]) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < size1 && j < size2) {
+        if (array1[i] < array2[j]) {
+            result[k++] = array1[i++];
+        } else {
+            result[k++] = array2[j++];
+        }
+    }
+
+    while (i < size1) {
+        result[k++] = array1[i++];
+    }
+
+    while (j < size2) {
+        result[k++] = array2[j++];
+    }
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int array1[] = {1, 3, 5, 7};
+    int array2[] = {2, 4, 6, 8};
+    int size1 = sizeof(array1) / sizeof(array1[0]);
+    int size2 = sizeof(array2) / sizeof(array2[0]);
+    cout << "Two array are \n";
+    cout << "First array: ";
+    printArray(array1, size1);
+    cout << "Second array: ";
+    printArray(array2, size2);
+    int result[size1 + size2];
+    mergeSortedArray(array1, size1, array2, size2, result);
+    cout << "After merge: ";
+    printArray(result, size1 + size2);
+}
