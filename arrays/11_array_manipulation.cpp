c71390d8f0f4838510422b819c7be65bc6420179
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-10-27 22:38:55 +0530

    broken intersection code - working on size of result arr

--- arrays/11_array_manipulation.cpp
+++ arrays/11_array_manipulation.cpp
@@ -22,6 +22,29 @@ void mergeSortedArray(int array1[], int size1, int array2[], int size2,
     }
 }
 
+void intersection(int array1[], int size1, int array2[], int size2,
+                  int result[]) {
+    int k = 0;
+
+    for (int i = 0; i < size1; i++) {
+        for (int j = 0; j < size2; j++) {
+            if (array1[i] == array2[j]) {
+                bool isAlreadyAdded = false;
+                for (int z = 0; z < k; z++) {
+                    if (result[z] == array1[i]) {
+                        isAlreadyAdded = true;
+                        break;
+                    }
+                }
+                if (!isAlreadyAdded) {
+                    result[k++] = array1[i];
+                }
+                break;
+            }
+        }
+    }
+}
+
 void printArray(int array[], int size) {
     for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
@@ -43,4 +66,15 @@ int main() {
     mergeSortedArray(array1, size1, array2, size2, result);
     cout << "After merge: ";
     printArray(result, size1 + size2);
+    cout << "\n\n";
+
+    // testing intersection fucntion
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {1, 3, 7, 8, 4};
+    int sz1 = sizeof(arr1) / sizeof(arr1[0]);
+    int sz2 = sizeof(arr2) / sizeof(arr2[0]);
+    cout << "commom elements are: ";
+    int result2[max(sz1, sz2)];
+    intersection(arr1, sz1, arr2, sz2, result2);
+    printArray(result2, max(sz1, sz2));
 }
