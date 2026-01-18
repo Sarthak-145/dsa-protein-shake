ab03035652a72e8c4f051924c5a491df9ae8cd67
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-01-18 12:02:28 +0530

    return index-array of previous smaller element

--- Stacks/03_NSPS.cpp
+++ Stacks/03_NSPS.cpp
@@ -18,11 +18,34 @@ vector<int> nextSmallerIdx(vector<int>& nums) {
     return nse;
 }
 
+vector<int> previousSmallerIdx(vector<int>& nums) {
+    stack<int> st;
+    vector<int> pse(nums.size(), -1);
+
+    for (int i=0; i< nums.size(); i++) {
+        while (!st.empty() && nums[i] < nums[st.top()]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            pse[i] = st.top();
+        }
+        st.push(i);
+    }
+    return pse;
+}
+
 int main() {
     vector<int> nums = {10, 2, 5, 4};
     vector<int> result = nextSmallerIdx(nums);
     cout << "Array containing indices of next smaller element is: ";
     for (int x: result) {
+        cout << x << " ";
+    }
+    cout << "\n\n";
+
+    vector<int> result2 = previousSmallerIdx(nums);
+    cout << "Array containing indices of previous smaller element is: ";
+    for (int x: result2) {
         cout << x << " ";
     }
     cout << "\n\n";
